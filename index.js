e62acaabcb3d7f3134c1d4c2690cbf89fe44f532
goldenriver2021 <goldenriver2021@gmail.com>
2021-06-04 14:29:22 +0700

    aa env

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'livechat'
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE
 });
 
 connection.connect((err) => {
